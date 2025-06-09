2558d938e286ecb95220a1ec6b3a5be66a9c0ab3
thiago1bd2 <thiagodaqui@gmail.com>
2025-06-08 22:25:47 -0300

    pyenv ajustes

--- aplicando_oo/restaurante.py
+++ aplicando_oo/restaurante.py
@@ -1,8 +1,25 @@
 class Restaurante:
+    restaurantes :list = []
+
     def __init__(self, nome :str, categoria :str) -> None:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self) -> str:
+        return f'Nome: {self.nome} | Categoria: {self.categoria} | Ativo: {
+            'sim' if self.ativo else 'não'}'
+    
+    def listar_restaurantes():
+        for r in Restaurante.restaurantes:
+            print(f'{r.nome}|{r.categoria}|{r.ativo}')
+
+    @property
+    def ativo(self):
+        return 'ativo' if self.ativo else 'desativado'
+            
+
 
 restaurante_praca = Restaurante('Praca','Gourmet')
 
@@ -10,8 +27,9 @@ restaurante_pizza = Restaurante('Pizza','Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+# print(restaurantes)
 
 # dir - lista todos metodos e attr da classe
 # vars - dic dos atributos
-print(vars(restaurante_praca))
+
+Restaurante.listar_restaurantes()
